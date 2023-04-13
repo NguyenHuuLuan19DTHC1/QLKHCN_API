71693484c54daa6f21bbadd2a83f3114f44e8a3a
nguyễn luân <ngluan161121@gmail.com>
2023-04-13 14:03:57 +0700

    Create

---
+++ QLKHCN_API/Controllers/DanhMucXetDuyetController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using QLKHCN_API.Data;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace QLKHCN_API.Controllers
+{
+    [Route("api/[controller]/")]
+    [ApiController]
+    public class DanhMucXetDuyetController : ControllerBase
+    {
+        private readonly MyDbContext _context;
+        public DanhMucXetDuyetController(MyDbContext context)
+        {
+            _context = context;
+
+        }
+        [HttpGet]
+        [Route("Get-all-datatem")]
+
+        public async Task<ActionResult<IEnumerable<DanhMucXetDuyet>>> GetAllDataTem()
+        {
+            try
+            {
+                return Ok(await _context.DanhMucXetDuyet.ToListAsync());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpGet]
+        [Route("Get-by-userid")]
+
+        public async Task<ActionResult<IEnumerable<DanhMucXetDuyet>>> GetByUserId(string userid)
+        {
+            var result = await _context.DanhMucXetDuyet.Where(a => a.userId == userid).ToListAsync();
+            try
+            {
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpGet]
+        [Route("Get-by-groupuser")]
+
+        public async Task<ActionResult<IEnumerable<DanhMucXetDuyet>>> GetByGroupUser(string groupuser)
+        {
+            var result = await _context.DanhMucXetDuyet.Where(a => a.userId.Contains(groupuser)).ToListAsync();
+            try
+            {
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpGet]
+        [Route("Get-by-tenbaibao")]
+
+        public async Task<ActionResult<IEnumerable<DanhMucXetDuyet>>> GetByTenBaiBao(string tenbaibao)
+        {
+            var result = await _context.DanhMucXetDuyet.Where(a => a.tenBaiBao.Contains(tenbaibao)).ToListAsync();
+            try
+            {
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ QLKHCN_API/Controllers/ThanhToanGVController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using QLKHCN_API.Data;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace QLKHCN_API.Controllers
+{
+    [Route("api/[controller]/")]
+    [ApiController]
+    public class ThanhToanGVController : ControllerBase
+    {
+        private readonly MyDbContext _context;
+        public ThanhToanGVController(MyDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        [Route("Get-LSP")]
+
+        public async Task<ActionResult<IEnumerable<ThanhToanGV>>> Get_LSP(string lsp)
+        {
+            try
+            {
+                var result = await _context.ThanhToanGV.Where(a => a.LoaiSanPham == lsp).ToListAsync();
+                if (result.Count > 0)
+                {
+                    return result;
+                }
+                return NotFound();
+            }
+
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpGet]
+        [Route("Get-ID")]
+
+        public async Task<ActionResult<IEnumerable<ThanhToanGV>>> Get_ID(int id)
+        {
+            try
+            {
+                var result = await _context.ThanhToanGV.Where(a => a.ID == id).ToListAsync();
+                if (result.Count > 0)
+                {
+                    return result;
+                }
+                return NotFound();
+            }
+
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
