6cd99fd4db8521b078f1dc6b178b1891f72ee5f5
thienphuc0108 <phucsky38@gmail.com>
2023-04-14 15:59:46 +0700

    LayoutAdmin

---
+++ layoutadmin/layoutadmin/Controllers/GiangVienController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace layoutadmin.Controllers
+{
+    public class GiangVienController : Controller
+    {
+        // GET: GiangVien
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
