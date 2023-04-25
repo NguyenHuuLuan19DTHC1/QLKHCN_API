4d09ffaffd0b96ddb0392856a072634ee2636b12
N.A.Tuan <taikhoanrac14789@gmail.com>
2023-04-25 16:06:51 +0700

    2

---
+++ layoutadmin/layoutadmin/Controllers/ArticleController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace layoutadmin.Controllers
+{
+    public class ArticleController : Controller
+    {
+        // GET: Article
+        public ActionResult GV_Page()
+        {
+            return View();
+        }
+
+        public ActionResult DeTai()
+        {
+            return View();
+        }
+
+        public ActionResult ThongTin()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ layoutadmin/layoutadmin/Controllers/DanhMucXetDuyetController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace layoutadmin.Controllers
+{
+    public class DanhMucXetDuyetController : Controller
+    {
+        // GET: DanhMucXetDuyet
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
